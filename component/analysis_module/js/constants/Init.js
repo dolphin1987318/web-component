0e0c0a835e4cd7ee64e53f4e2e5c9a150022c7e6
Allen <allen_wangxufeng@163.com>
2015-08-11 10:21:47 +0800

    tmp

---
+++ component/analysis_module/js/constants/Init.js
@@ -0,0 +1,9 @@
+/**
+ * Created by wangxufeng on 2015/8/6.
+ */
+
+module.exports = {
+    INIT_URL: '../assets/data/dcenter/analysis_module/mod_lost.json',
+    DATA_URL: '../assets/data/dcenter/analysis_module/simulate_mod_lost.json',
+    IMG_PATH: '../assets/img/'
+};
